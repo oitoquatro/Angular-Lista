1da77396a18a34d2a352f031b4b7246c2870185e
PJPJunior <paulojpjunior@gmail.com>
2023-01-03 19:04:58 -0300

    Lista de alunos
    utilizando angular, e API randomuser.me para uma listagem randômica com nome, sexo e nacionalidade.

---
+++ desafio_angular/src/app/Core/services/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { enviroment } from '../../../../assets/enviroments/enviroment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+  getUsers(quantityUsers : string): Observable<any>{
+    return this.http.get(`${enviroment.userApiUrl}?results=${quantityUsers}`)
+  }
+}
